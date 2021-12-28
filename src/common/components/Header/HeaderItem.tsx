a68f9cae69b0eb52b9f13d4c3f126e2bb5b24b06
adham618 <adhamtarek291@gmail.com>
2021-12-28 20:52:08 +0200

    create HeaderItems components

---
+++ src/common/components/Header/HeaderItem.tsx
@@ -0,0 +1,17 @@
+import { ReactElement, SVGProps } from "react"
+
+interface HeaderItemProps {
+  title: string,
+  Icon: (props: SVGProps<SVGSVGElement>) => ReactElement<any, any>
+}
+
+
+const HeaderItem = ({ title, Icon }: HeaderItemProps) => {
+  return (
+    <div className="flex flex-col items-center cursor-pointer hover:text-white group  w:12 sm:w-20">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
+    </div>
+  )
+}
+export default HeaderItem
