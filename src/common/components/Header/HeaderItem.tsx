4e24a94c7953c8c70a69a94dc2e7388520e1a328
adham618 <adhamtarek291@gmail.com>
2021-12-30 16:49:40 +0200

    fix extra width in mobile screen

--- src/common/components/Header/HeaderItem.tsx
+++ src/common/components/Header/HeaderItem.tsx
@@ -8,9 +8,9 @@ interface HeaderItemProps {
 
 const HeaderItem = ({ title, Icon }: HeaderItemProps) => {
   return (
-    <div className="flex flex-col items-center cursor-pointer hover:text-white group  w:12 sm:w-20">
-      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-      <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
+    <div className="flex flex-col items-center cursor-pointer hover:text-white group  w-12 sm:w-20">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce -tracking-widest" />
+      <p className="opacity-0 group-hover:opacity-100 ">{title}</p>
     </div>
   )
 }
