5ba7620712a396ee90ffc1a56446e7cb92a65b2c
adham618 <adhamtarek291@gmail.com>
2021-12-30 16:19:59 +0200

    fix typescript errors

--- src/common/utils/requests.tsx
+++ src/common/utils/requests.tsx
@@ -1,49 +1,56 @@
 const API_KEY = process.env.API_KEY
+export interface IRequest {
+  [name: string]: {
+    title: string;
+    url: string;
+  };
+}
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+const data: IRequest = {
   fetchTrending: {
     title: "Trending",
-    url: "/trending/all/week?api_key=${API_KEY}&language=en-US"
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
   },
   fetchTopRated: {
     title: "Top Rated",
-    url: "/discover/movie?api_key=${API_KEY}&language=en-US"
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US`
   },
   fetchActionMovies: {
     title: "Action",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=28"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
   },
   fetchComedyMovies: {
     title: "Comdey",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=35"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
   },
   fetchHorrorMovies: {
     title: "Horror",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=27"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
   },
   fetchRomanceMovies: {
     title: "Romance",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=10740"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10740`
   },
   fetchMystery: {
     title: "Mystery",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=9648"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
   },
   fetchSciFi: {
     title: "Sci-Fi",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=37878"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37878`
   },
   fetchWestern: {
     title: "Western",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=37"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
   },
   fetchAnimations: {
     title: "Animation",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=16"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
   },
   fetchTv: {
     title: "Tv Movie",
-    url: "/discover/movie?api_key=${API_KEY}&with_genres=10770"
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
   }
 }
+
+export default data
