b5a630f9292b84b2ba0d86d03bdfba2ed31736c0
adham618 <adhamtarek291@gmail.com>
2021-12-30 16:20:51 +0200

    fix typescript errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../common/components/Header/Header'
 import Navbar from '../common/components/Navbar/Navbar'
-import Requests from '../common/components/Requests/Requests'
+import Results from '../common/components/Results/Results'
+import requests from '../common/utils/requests'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  results: []
+
+}
+
+const Home: NextPage<HomeProps> = ({ results }: HomeProps) => {
+  //console.log(results)
   return (
     <>
       <Head>
@@ -15,9 +21,23 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <Navbar />
-      <Requests />
+      <Results results={results} />
     </>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const genre = context.query.genre as string
+  const res = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
+  )
+  const data = await res.json()
+  return {
+    props: {
+      results: data.results
+    }
+  }
+}
+
